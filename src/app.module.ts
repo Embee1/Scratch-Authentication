eea9e13ec89d90a1ae9420e444cad7ec63f330fd
Embee1 <akpanmbietidughe@gmail.com>
2022-11-02 11:11:23 +0100

    added new feature to typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,7 @@ import { ConfigModule } from '@nestjs/config';
       database: 'd1hif7lgg4ur1o',
       entities: [Coffee, Flavor, UserEntity],
       synchronize: true,
+      ssl: true
     }),
 
     ConfigModule.forRoot(),
